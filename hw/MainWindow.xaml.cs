15c78f64c64c11b038a2ee6e1ba17586722a0f2c
Турмахан Динислам Даулетулы <@corp.itstep>
2019-06-25 15:14:18 +0600

    first commit

---
+++ hw/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+
+namespace hw
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                IPHostEntry hostAddress = Dns.GetHostByName(textBox.Text);
+                foreach (var item in hostAddress.AddressList)
+                {
+                  ipAddressTextBox.Text += item.ToString() + "\n";
+                }
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+    }
+}
